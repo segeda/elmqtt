beec15b9aafc62099a26e63ca165cb9d84c5079f
Petr Severa <segedacz@gmail.com>
2017-02-10 14:34:31 +0100

    JSON payload to Elm

--- main.js
+++ main.js
@@ -1,20 +1,11 @@
 'use strict';
 
-var elm = Elm.Main.embed(document.getElementById('app'));
+var elm = Elm.Main.fullscreen();
 
 var client = mqtt.connect('<MQTT-SERVER>');
 
-var root_topic = 'arduino/yun/';
-client.subscribe(root_topic + '#');
+client.subscribe('arduino/yun');
 
 client.on('message', function (topic, payload) {
-    if (topic === root_topic + 'humidity') {
-        document.getElementById('humidity').innerText = payload + "%";
-    }
-    if (topic === root_topic + 'temperature') {
-        document.getElementById('temperature').innerText = payload + "°C";
-    }
-    if (topic === root_topic + 'heatIndex') {
-        document.getElementById('heatIndex').innerText = payload + "°C";
-    }
+    elm.ports.onMessage.send(JSON.parse(payload));
 });
--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,88 @@
-import Html exposing (h1, text)
+port module Main exposing (main)
 
-main : Html.Html msg
+import Html exposing (Html, h1, div, dl, dt, dd, text)
+
+
+main : Program Never Model Msg
 main =
-  h1 [] [ text "elmqtt"]
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    Payload
+
+
+type alias Payload =
+    { humidity : Int
+    , temperature : Int
+    , heatIndex : Float
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( Payload 0 0 0.0, Cmd.none )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = OnMessage Payload
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        OnMessage payload ->
+            ( payload, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+port onMessage : (Payload -> msg) -> Sub msg
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    onMessage OnMessage
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h1 [] [ text "Elm and MQTT" ]
+        , dl []
+            [ dt [] [ text "humidity" ]
+            , dd []
+                [ text (toString model.humidity)
+                , text "%"
+                ]
+            , dt [] [ text "temperature" ]
+            , dd []
+                [ text (toString model.temperature)
+                , text "°C"
+                ]
+            , dt [] [ text "heatIndex" ]
+            , dd []
+                [ text (toString model.heatIndex)
+                , text "°C"
+                ]
+            ]
+        ]
